a8acb2533848e7c27ab7b7fdff8776bd644035da
Zhaoning Cai <zcai9@my.centennialcollege.ca>
2021-06-29 17:53:22 -0400

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
+import React, {useEffect, Fragment} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 
+import Navbar from './components/layout/Navbar';
+
 const App = () => {
+  useEffect(() => {
+    M.AutoInit();
+  });
   return (
-    <div className="App">
-      Portfolio
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className="container">
+          
+        </div>
+      </Fragment>
+    </Router>
   );
 }
 
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,41 @@
+import React, {Fragment} from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <Fragment>
+            <ul id="gameDropdown" className="dropdown-content">
+                <li><Link to="/games/tower-defense">Battle City TD</Link></li>
+                <li><Link to="/games/mario">Super Mario 1-1</Link></li>
+                <li><Link to="/games/treasure-hunter">Treasure Hunter</Link></li>
+                <li><Link to="/games/jailbreak">Jailbreak 3D</Link></li>
+            </ul>
+            <nav>
+                <div className="nav-wrapper">
+                    <Link to="/" className="brand-logo">Zhaoning(Kyle) Cai Portfolio</Link>
+                    <a href="#!" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+                    <ul className="right hide-on-med-and-down">
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/website">Website</Link></li>
+                        <li><a className="dropdown-trigger" href="#!" data-target="gameDropdown">Games<i className="material-icons right">arrow_drop_down</i></a></li>
+                    </ul>
+                </div>
+            </nav>
+
+            <ul className="sidenav" id="mobile-demo">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/website">Website</Link></li>
+                <li>Games
+                    <ul>
+                        <li><Link to="/games/tower-defense">Battle City TD</Link></li>
+                        <li><Link to="/games/mario">Super Mario 1-1</Link></li>
+                        <li><Link to="/games/treasure-hunter">Treasure Hunter</Link></li>
+                        <li><Link to="/games/jailbreak">Jailbreak 3D</Link></li>
+                    </ul>
+                </li>
+            </ul>
+        </Fragment>
+    )
+}
+
+export default Navbar;
